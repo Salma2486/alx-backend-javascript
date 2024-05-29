90efe5481185ce5de4992ca8bf97a3530a397db1
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-05-29 13:32:04 +0000

    g

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,13 @@
 export default function guardrail(mathFunction) {
-  const queue = [];
-  try {
-    queue.push(mathFunction());
-  } catch (err) {
-    queue.push(String(err));
-  } finally {
-    queue.push('Guardrail was processed');
+    const queue = [];
+  
+    try {
+      queue.push(mathFunction());
+    } catch (err) {
+      queue.push(String(err));
+    } finally {
+      queue.push('Guardrail was processed');
+    }
+  
+    return queue;
   }
-  return queue;
-}
