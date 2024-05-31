5a6a9f8899eccd2e2eab3ace3ccf6b1184c2a75c
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-05-31 01:16:19 +0000

    q

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,20 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqfr, floors) {
+    super(sqfr);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors!`;
+  }
+}
