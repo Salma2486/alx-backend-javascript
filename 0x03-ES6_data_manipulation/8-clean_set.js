4c59b697d26125da011e0d31613ca4cfd2f5f3e8
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-06-03 16:09:00 +0000

    t

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,12 @@
 export default function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-        return '';
-      }
-    
-      let result = '';
-      for (const item of set) {
-        if (item && item.startsWith(startString)) {
-          result += `${item.slice(startString.length)}-`;
-        }
-      }
-    
-      return result.slice(0, -1); // Remove the trailing '-'
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  let result = '';
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1);
 }
